7cf2e4692d0a8a40bc30a7a493f4863a01c923bd
Jenil Makwana <99378825+j-makwana@users.noreply.github.com>
2025-07-21 14:31:40 +0530

    customers done

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,32 @@
+import CustomersTable from "@/app/ui/customers/table";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+import { fetchFilteredCustomers } from "@/app/lib/data";
 import { Metadata } from "next";
+
 export const metadata: Metadata = {
-  title: "customers",
+  title: "Customers",
 };
-export default function Page() {
-  return <p>Customers Page</p>;
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+
+  return (
+    <div className="w-full">
+      <Suspense key={query} fallback={<InvoicesTableSkeleton />}>
+        <CustomersTableWrapper query={query} />
+      </Suspense>
+    </div>
+  );
+}
+
+async function CustomersTableWrapper({ query }: { query: string }) {
+  const customers = await fetchFilteredCustomers(query);
+
+  return <CustomersTable customers={customers} />;
 }
